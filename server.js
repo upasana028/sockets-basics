69be7b833fbbbaa74f7d3a3e49c7c4f23699b59f
upasana028 <028upasana820@gmail.com>
2016-01-26 18:23:55 +0530

    client listener added

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var io = require('socket.io')(http);
 app.use(express.static(__dirname +'/public'));
 
 io.on('connection',function(){
-	console.log('User connection via socket');
+	console.log('User connection via socket::Server message');
 });
 http.listen(PORT,function(){
 	console.log("Server started!");
